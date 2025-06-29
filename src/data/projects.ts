d171622acd159615511a74b248f63e2e587a3949
mariusurucu <urucumarius@yahoo.com>
2025-06-30 00:03:04 +0300

    Adding overlay and filtering functionalities.

---
+++ src/data/projects.ts
@@ -0,0 +1,283 @@
+// AP_1
+import ap1_1 from '../assets/fulls/ap1/1.jpg';
+import ap1_2 from '../assets/fulls/ap1/2.jpg';
+import ap1_3 from '../assets/fulls/ap1/3.jpg';
+import ap1_4 from '../assets/fulls/ap1/4.jpg';
+
+// AP_4
+import ap4_1 from '../assets/fulls/ap4/1.jpg';
+import ap4_2 from '../assets/fulls/ap4/2.jpg';
+import ap4_3 from '../assets/fulls/ap4/3.jpg';
+import ap4_4 from '../assets/fulls/ap4/4.jpg';
+import ap4_5 from '../assets/fulls/ap4/5.jpg';
+import ap4_6 from '../assets/fulls/ap4/6.jpg';
+import ap4_7 from '../assets/fulls/ap4/7.jpg';
+import ap4_8 from '../assets/fulls/ap4/8.jpg';
+import ap4_9 from '../assets/fulls/ap4/9.jpg';
+import ap4_10 from '../assets/fulls/ap4/10.jpg';
+import ap4_11 from '../assets/fulls/ap4/11.jpg';
+import ap4_12 from '../assets/fulls/ap4/12.jpg';
+
+// AP_6
+import ap6_1 from '../assets/fulls/ap6/01.jpg';
+import ap6_2 from '../assets/fulls/ap6/02.jpg';
+import ap6_3 from '../assets/fulls/ap6/03.jpg';
+import ap6_4 from '../assets/fulls/ap6/04.jpg';
+import ap6_5 from '../assets/fulls/ap6/05.jpg';
+import ap6_6 from '../assets/fulls/ap6/06.jpg';
+import ap6_7 from '../assets/fulls/ap6/07.jpg';
+
+// AP_8
+import ap8_1 from '../assets/fulls/ap8/1.jpg';
+import ap8_2 from '../assets/fulls/ap8/2.jpg';
+import ap8_3 from '../assets/fulls/ap8/3.jpg';
+import ap8_4 from '../assets/fulls/ap8/4.jpg';
+import ap8_5 from '../assets/fulls/ap8/5.jpg';
+import ap8_6 from '../assets/fulls/ap8/6.jpg';
+import ap8_7 from '../assets/fulls/ap8/7.jpg';
+import ap8_8 from '../assets/fulls/ap8/8.jpg';
+import ap8_9 from '../assets/fulls/ap8/9.jpg';
+import ap8_10 from '../assets/fulls/ap8/10.jpg';
+
+// AP_9
+import ap9_1 from '../assets/fulls/ap9/1.jpg';
+import ap9_2 from '../assets/fulls/ap9/2.jpg';
+import ap9_3 from '../assets/fulls/ap9/3.jpg';
+import ap9_4 from '../assets/fulls/ap9/4.jpg';
+import ap9_5 from '../assets/fulls/ap9/5.jpg';
+import ap9_6 from '../assets/fulls/ap9/6.jpg';
+import ap9_7 from '../assets/fulls/ap9/7.jpg';
+import ap9_8 from '../assets/fulls/ap9/8.jpg';
+import ap9_9 from '../assets/fulls/ap9/9.jpg';
+import ap9_10 from '../assets/fulls/ap9/10.jpg';
+import ap9_11 from '../assets/fulls/ap9/11.jpg';
+
+// AP_WBISBI
+import apwbisbi_1 from '../assets/fulls/apwbisbi/1.jpg';
+import apwbisbi_2 from '../assets/fulls/apwbisbi/2.jpg';
+import apwbisbi_3 from '../assets/fulls/apwbisbi/3.jpg';
+import apwbisbi_4 from '../assets/fulls/apwbisbi/4.jpg';
+import apwbisbi_5 from '../assets/fulls/apwbisbi/5.jpg';
+import apwbisbi_6 from '../assets/fulls/apwbisbi/6.jpg';
+import apwbisbi_7 from '../assets/fulls/apwbisbi/7.jpg';
+import apwbisbi_8 from '../assets/fulls/apwbisbi/8.jpg';
+import apwbisbi_9 from '../assets/fulls/apwbisbi/9.jpg';
+import apwbisbi_10 from '../assets/fulls/apwbisbi/10.jpg';
+import apwbisbi_11 from '../assets/fulls/apwbisbi/11.jpg';
+import apwbisbi_12 from '../assets/fulls/apwbisbi/12.jpg';
+import apwbisbi_13 from '../assets/fulls/apwbisbi/13.jpg';
+import apwbisbi_14 from '../assets/fulls/apwbisbi/14.jpg';
+import apwbisbi_15 from '../assets/fulls/apwbisbi/15.jpg';
+
+// BARBER_SHOP_X
+import barbershopx_1 from '../assets/fulls/barbershopx/1.jpg';
+import barbershopx_2 from '../assets/fulls/barbershopx/2.jpg';
+import barbershopx_3 from '../assets/fulls/barbershopx/3.jpg';
+import barbershopx_4 from '../assets/fulls/barbershopx/4.jpg';
+import barbershopx_5 from '../assets/fulls/barbershopx/5.jpg';
+import barbershopx_6 from '../assets/fulls/barbershopx/6.jpg';
+import barbershopx_7 from '../assets/fulls/barbershopx/7.jpg';
+import barbershopx_8 from '../assets/fulls/barbershopx/8.jpg';
+
+// CASA_IRD
+import CASA_IRD_1 from '../assets/fulls/CASA_IRD/1.jpg';
+import CASA_IRD_2 from '../assets/fulls/CASA_IRD/2.jpg';
+import CASA_IRD_3 from '../assets/fulls/CASA_IRD/3.jpg';
+import CASA_IRD_4 from '../assets/fulls/CASA_IRD/4.jpg';
+import CASA_IRD_5 from '../assets/fulls/CASA_IRD/5.jpg';
+import CASA_IRD_6 from '../assets/fulls/CASA_IRD/D1.jpg';
+import CASA_IRD_7 from '../assets/fulls/CASA_IRD/D2.jpg';
+import CASA_IRD_8 from '../assets/fulls/CASA_IRD/D3.jpg';
+
+// SHISHA_BAR
+import SHISHA_BAR_1 from '../assets/fulls/SHISHA_BAR/1.jpg';
+import SHISHA_BAR_2 from '../assets/fulls/SHISHA_BAR/2.jpg';
+import SHISHA_BAR_3 from '../assets/fulls/SHISHA_BAR/3.jpg';
+import SHISHA_BAR_4 from '../assets/fulls/SHISHA_BAR/4.jpg';
+
+// MDR_INT
+import MDR_INT_1 from '../assets/fulls/MDR_INT/1.jpg';
+import MDR_INT_2 from '../assets/fulls/MDR_INT/2.jpg';
+import MDR_INT_3 from '../assets/fulls/MDR_INT/3.jpg';
+import MDR_INT_4 from '../assets/fulls/MDR_INT/4.jpg';
+import MDR_INT_5 from '../assets/fulls/MDR_INT/5.jpg';
+
+// DRM_1
+import DRM_1 from '../assets/fulls/DRM/1.jpg';
+import DRM_2 from '../assets/fulls/DRM/2.jpg';
+import DRM_3 from '../assets/fulls/DRM/3.jpg';
+import DRM_4 from '../assets/fulls/DRM/4.jpg';
+import DRM_5 from '../assets/fulls/DRM/6.jpg';
+
+// DRM_2
+import DRM2_1 from '../assets/fulls/DRM_2/1.jpg';
+import DRM2_2 from '../assets/fulls/DRM_2/2.jpg';
+import DRM2_3 from '../assets/fulls/DRM_2/3.jpg';
+import DRM2_4 from '../assets/fulls/DRM_2/4.jpg';
+
+// LVG_1
+import LVG1_1 from '../assets/fulls/LVG1/1.jpg';
+import LVG1_2 from '../assets/fulls/LVG1/2.jpg';
+import LVG1_3 from '../assets/fulls/LVG1/3.jpg';
+import LVG1_4 from '../assets/fulls/LVG1/4.jpg';
+import LVG1_5 from '../assets/fulls/LVG1/5.jpg';
+
+// LVG_2
+import LVG2_1 from '../assets/fulls/LVG2/1.jpg';
+import LVG2_2 from '../assets/fulls/LVG2/2.jpg';
+import LVG2_3 from '../assets/fulls/LVG2/3.jpg';
+import LVG2_4 from '../assets/fulls/LVG2/4.jpg';
+
+// LVG_3
+import LVG3_1 from '../assets/fulls/LVG3/1.jpg';
+import LVG3_2 from '../assets/fulls/LVG3/2.jpg';
+import LVG3_3 from '../assets/fulls/LVG3/3.jpg';
+
+// KTC_2
+import KTC2_1 from '../assets/fulls/KTC2/1.jpg';
+import KTC2_2 from '../assets/fulls/KTC2/2.jpg';
+import KTC2_3 from '../assets/fulls/KTC2/3.jpg';
+
+// BTH_1
+import bth1_1 from '../assets/fulls/bth1/1.jpg';
+import bth1_2 from '../assets/fulls/bth1/2.jpg';
+
+// BTH_3
+import bth3_1 from '../assets/fulls/bth3/1.jpg';
+import bth3_2 from '../assets/fulls/bth3/2.jpg';
+import bth3_3 from '../assets/fulls/bth3/3.jpg';
+
+// Data
+export const projects = [
+  // AP_1
+  { id: "ap1_1", group: "AP_1", image: ap1_1, title: "AP_1" },
+  { id: "ap1_2", group: "AP_1", image: ap1_2, title: "AP_1" },
+  { id: "ap1_3", group: "AP_1", image: ap1_3, title: "AP_1" },
+  { id: "ap1_4", group: "AP_1", image: ap1_4, title: "AP_1" },
+  // AP_4
+  { id: "ap4_1", group: "AP_4", image: ap4_1, title: "AP_4" },
+  { id: "ap4_2", group: "AP_4", image: ap4_2, title: "AP_4" },
+  { id: "ap4_3", group: "AP_4", image: ap4_3, title: "AP_4" },
+  { id: "ap4_4", group: "AP_4", image: ap4_4, title: "AP_4" },
+  { id: "ap4_5", group: "AP_4", image: ap4_5, title: "AP_4" },
+  { id: "ap4_6", group: "AP_4", image: ap4_6, title: "AP_4" },
+  { id: "ap4_7", group: "AP_4", image: ap4_7, title: "AP_4" },
+  { id: "ap4_8", group: "AP_4", image: ap4_8, title: "AP_4" },
+  { id: "ap4_9", group: "AP_4", image: ap4_9, title: "AP_4" },
+  { id: "ap4_10", group: "AP_4", image: ap4_10, title: "AP_4" },
+  { id: "ap4_11", group: "AP_4", image: ap4_11, title: "AP_4" },
+  { id: "ap4_12", group: "AP_4", image: ap4_12, title: "AP_4" },
+  // AP_6
+  { id: "ap6_1", group: "AP_6", image: ap6_1, title: "AP_6" },
+  { id: "ap6_2", group: "AP_6", image: ap6_2, title: "AP_6" },
+  { id: "ap6_3", group: "AP_6", image: ap6_3, title: "AP_6" },
+  { id: "ap6_4", group: "AP_6", image: ap6_4, title: "AP_6" },
+  { id: "ap6_5", group: "AP_6", image: ap6_5, title: "AP_6" },
+  { id: "ap6_6", group: "AP_6", image: ap6_6, title: "AP_6" },
+  { id: "ap6_7", group: "AP_6", image: ap6_7, title: "AP_6" },
+  // AP_8
+  { id: "ap8_1", group: "AP_8", image: ap8_1, title: "AP_8" },
+  { id: "ap8_2", group: "AP_8", image: ap8_2, title: "AP_8" },
+  { id: "ap8_3", group: "AP_8", image: ap8_3, title: "AP_8" },
+  { id: "ap8_4", group: "AP_8", image: ap8_4, title: "AP_8" },
+  { id: "ap8_5", group: "AP_8", image: ap8_5, title: "AP_8" },
+  { id: "ap8_6", group: "AP_8", image: ap8_6, title: "AP_8" },
+  { id: "ap8_7", group: "AP_8", image: ap8_7, title: "AP_8" },
+  { id: "ap8_8", group: "AP_8", image: ap8_8, title: "AP_8" },
+  { id: "ap8_9", group: "AP_8", image: ap8_9, title: "AP_8" },
+  { id: "ap8_10", group: "AP_8", image: ap8_10, title: "AP_8" },
+  // AP_9
+  { id: "ap9_1", group: "AP_9", image: ap9_1, title: "AP_9" },
+  { id: "ap9_2", group: "AP_9", image: ap9_2, title: "AP_9" },
+  { id: "ap9_3", group: "AP_9", image: ap9_3, title: "AP_9" },
+  { id: "ap9_4", group: "AP_9", image: ap9_4, title: "AP_9" },
+  { id: "ap9_5", group: "AP_9", image: ap9_5, title: "AP_9" },
+  { id: "ap9_6", group: "AP_9", image: ap9_6, title: "AP_9" },
+  { id: "ap9_7", group: "AP_9", image: ap9_7, title: "AP_9" },
+  { id: "ap9_8", group: "AP_9", image: ap9_8, title: "AP_9" },
+  { id: "ap9_9", group: "AP_9", image: ap9_9, title: "AP_9" },
+  { id: "ap9_10", group: "AP_9", image: ap9_10, title: "AP_9" },
+  { id: "ap9_11", group: "AP_9", image: ap9_11, title: "AP_9" },
+  // AP_WBISBI
+  { id: "apwbisbi_1", group: "AP_WBISBI", image: apwbisbi_1, title: "AP_WBISBI" },
+  { id: "apwbisbi_2", group: "AP_WBISBI", image: apwbisbi_2, title: "AP_WBISBI" },
+  { id: "apwbisbi_3", group: "AP_WBISBI", image: apwbisbi_3, title: "AP_WBISBI" },
+  { id: "apwbisbi_4", group: "AP_WBISBI", image: apwbisbi_4, title: "AP_WBISBI" },
+  { id: "apwbisbi_5", group: "AP_WBISBI", image: apwbisbi_5, title: "AP_WBISBI" },
+  { id: "apwbisbi_6", group: "AP_WBISBI", image: apwbisbi_6, title: "AP_WBISBI" },
+  { id: "apwbisbi_7", group: "AP_WBISBI", image: apwbisbi_7, title: "AP_WBISBI" },
+  { id: "apwbisbi_8", group: "AP_WBISBI", image: apwbisbi_8, title: "AP_WBISBI" },
+  { id: "apwbisbi_9", group: "AP_WBISBI", image: apwbisbi_9, title: "AP_WBISBI" },
+  { id: "apwbisbi_10", group: "AP_WBISBI", image: apwbisbi_10, title: "AP_WBISBI" },
+  { id: "apwbisbi_11", group: "AP_WBISBI", image: apwbisbi_11, title: "AP_WBISBI" },
+  { id: "apwbisbi_12", group: "AP_WBISBI", image: apwbisbi_12, title: "AP_WBISBI" },
+  { id: "apwbisbi_13", group: "AP_WBISBI", image: apwbisbi_13, title: "AP_WBISBI" },
+  { id: "apwbisbi_14", group: "AP_WBISBI", image: apwbisbi_14, title: "AP_WBISBI" },
+  { id: "apwbisbi_15", group: "AP_WBISBI", image: apwbisbi_15, title: "AP_WBISBI" },
+  // BARBER_SHOP_X
+  { id: "barbershopx_1", group: "BARBER_SHOP_X", image: barbershopx_1, title: "BARBER_SHOP_X" },
+  { id: "barbershopx_2", group: "BARBER_SHOP_X", image: barbershopx_2, title: "BARBER_SHOP_X" },
+  { id: "barbershopx_3", group: "BARBER_SHOP_X", image: barbershopx_3, title: "BARBER_SHOP_X" },
+  { id: "barbershopx_4", group: "BARBER_SHOP_X", image: barbershopx_4, title: "BARBER_SHOP_X" },
+  { id: "barbershopx_5", group: "BARBER_SHOP_X", image: barbershopx_5, title: "BARBER_SHOP_X" },
+  { id: "barbershopx_6", group: "BARBER_SHOP_X", image: barbershopx_6, title: "BARBER_SHOP_X" },
+  { id: "barbershopx_7", group: "BARBER_SHOP_X", image: barbershopx_7, title: "BARBER_SHOP_X" },
+  { id: "barbershopx_8", group: "BARBER_SHOP_X", image: barbershopx_8, title: "BARBER_SHOP_X" },
+  // CASA_IRD
+  { id: "CASA_IRD_1", group: "CASA_IRD", image: CASA_IRD_1, title: "CASA_IRD" },
+  { id: "CASA_IRD_2", group: "CASA_IRD", image: CASA_IRD_2, title: "CASA_IRD" },
+  { id: "CASA_IRD_3", group: "CASA_IRD", image: CASA_IRD_3, title: "CASA_IRD" },
+  { id: "CASA_IRD_4", group: "CASA_IRD", image: CASA_IRD_4, title: "CASA_IRD" },
+  { id: "CASA_IRD_5", group: "CASA_IRD", image: CASA_IRD_5, title: "CASA_IRD" },
+  { id: "CASA_IRD_6", group: "CASA_IRD", image: CASA_IRD_6, title: "CASA_IRD" },
+  { id: "CASA_IRD_7", group: "CASA_IRD", image: CASA_IRD_7, title: "CASA_IRD" },
+  { id: "CASA_IRD_8", group: "CASA_IRD", image: CASA_IRD_8, title: "CASA_IRD" },
+  // SHISHA_BAR
+  { id: "SHISHA_BAR_1", group: "SHISHA_BAR", image: SHISHA_BAR_1, title: "SHISHA_BAR" },
+  { id: "SHISHA_BAR_2", group: "SHISHA_BAR", image: SHISHA_BAR_2, title: "SHISHA_BAR" },
+  { id: "SHISHA_BAR_3", group: "SHISHA_BAR", image: SHISHA_BAR_3, title: "SHISHA_BAR" },
+  { id: "SHISHA_BAR_4", group: "SHISHA_BAR", image: SHISHA_BAR_4, title: "SHISHA_BAR" },
+  // MDR_INT
+  { id: "MDR_INT_1", group: "MDR_INT", image: MDR_INT_1, title: "MDR_INT" },
+  { id: "MDR_INT_2", group: "MDR_INT", image: MDR_INT_2, title: "MDR_INT" },
+  { id: "MDR_INT_3", group: "MDR_INT", image: MDR_INT_3, title: "MDR_INT" },
+  { id: "MDR_INT_4", group: "MDR_INT", image: MDR_INT_4, title: "MDR_INT" },
+  { id: "MDR_INT_5", group: "MDR_INT", image: MDR_INT_5, title: "MDR_INT" },
+  // DRM_1
+  { id: "DRM_1", group: "DRM_1", image: DRM_1, title: "DRM_1" },
+  { id: "DRM_2", group: "DRM_1", image: DRM_2, title: "DRM_1" },
+  { id: "DRM_3", group: "DRM_1", image: DRM_3, title: "DRM_1" },
+  { id: "DRM_4", group: "DRM_1", image: DRM_4, title: "DRM_1" },
+  { id: "DRM_5", group: "DRM_1", image: DRM_5, title: "DRM_1" },
+  // DRM_2
+  { id: "DRM2_1", group: "DRM_2", image: DRM2_1, title: "DRM_2" },
+  { id: "DRM2_2", group: "DRM_2", image: DRM2_2, title: "DRM_2" },
+  { id: "DRM2_3", group: "DRM_2", image: DRM2_3, title: "DRM_2" },
+  { id: "DRM2_4", group: "DRM_2", image: DRM2_4, title: "DRM_2" },
+  // LVG_1
+  { id: "LVG1_1", group: "LVG_1", image: LVG1_1, title: "LVG_1" },
+  { id: "LVG1_2", group: "LVG_1", image: LVG1_2, title: "LVG_1" },
+  { id: "LVG1_3", group: "LVG_1", image: LVG1_3, title: "LVG_1" },
+  { id: "LVG1_4", group: "LVG_1", image: LVG1_4, title: "LVG_1" },
+  { id: "LVG1_5", group: "LVG_1", image: LVG1_5, title: "LVG_1" },
+  // LVG_2
+  { id: "LVG2_1", group: "LVG_2", image: LVG2_1, title: "LVG_2" },
+  { id: "LVG2_2", group: "LVG_2", image: LVG2_2, title: "LVG_2" },
+  { id: "LVG2_3", group: "LVG_2", image: LVG2_3, title: "LVG_2" },
+  { id: "LVG2_4", group: "LVG_2", image: LVG2_4, title: "LVG_2" },
+  // LVG_3
+  { id: "LVG3_1", group: "LVG_3", image: LVG3_1, title: "LVG_3" },
+  { id: "LVG3_2", group: "LVG_3", image: LVG3_2, title: "LVG_3" },
+  { id: "LVG3_3", group: "LVG_3", image: LVG3_3, title: "LVG_3" },
+  // KTC_2
+  { id: "KTC2_1", group: "KTC_2", image: KTC2_1, title: "KTC_2" },
+  { id: "KTC2_2", group: "KTC_2", image: KTC2_2, title: "KTC_2" },
+  { id: "KTC2_3", group: "KTC_2", image: KTC2_3, title: "KTC_2" },
+  // BTH_1
+  { id: "bth1_1", group: "BTH_1", image: bth1_1, title: "BTH_1" },
+  { id: "bth1_2", group: "BTH_1", image: bth1_2, title: "BTH_1" },
+  // BTH_3
+  { id: "bth3_1", group: "BTH_3", image: bth3_1, title: "BTH_3" },
+  { id: "bth3_2", group: "BTH_3", image: bth3_2, title: "BTH_3" },
+  { id: "bth3_3", group: "BTH_3", image: bth3_3, title: "BTH_3" },
+];
